a690f95caf3daa2223bd26a0437d247aeda23c65
Kent C. Dodds <kent+github@doddsfamily.us>
2015-01-23 14:27:58 -0700

    Update [timestamp] ʕ •ᴥ•ʔ

--- index.js
+++ index.js
@@ -1,58 +1,38 @@
-let originalBackground;
-let originalBackgroundSize;
-let el;
+/** @jsx React.DOM */
+var React = require('react');
 
-module.exports = rbb;
-module.exports.off = off;
+require('./global.less');
+
+var toggleRbb = getToggleRbb();
+toggleRbb(); // turn it on
+
+var Card = require('./components/Card');
 
 
-function rbb({
-  root = 'rbb_backgrounds',
-  count,
-  extension = 'png',
-  prefix = 'bg',
-  images,
-  element = document.documentElement
-  } = {}) {
-  if (Number.isInteger(arguments[0])) {
-    count = arguments[0];
-  } else if (Array.isArray(arguments[0])) {
-    images = arguments[0];
+var App = React.createClass({
+  render() {
+    return (
+      <div className="animated fadeInUp">
+        <Card toggleBackground={toggleRbb} />
+      </div>
+    )
   }
-  if (!images && !count) {
-    throw new Error('rbb: You must either specify images or count');
-  }
+});
 
-  let background;
-  if (images) {
-    background = rand(images);
-  } else {
-    background = `${root}/${prefix}${rand(count - 1)}.${extension}`;
-  }
+React.render(<App />, document.body);
 
-  // set internal state
-  el = element;
-  originalBackground = el.style.background;
-  originalBackgroundSize = el.style['background-size'];
 
-  // set the styles
-  el.style.background = 'url("' + background + '") no-repeat center center fixed';
-  el.style['background-size'] = 'cover';
-  return module.exports;
-}
 
-function off() {
-  el.style.background = originalBackground;
-  el.style['background-size'] = originalBackgroundSize;
-  return module.exports;
-}
-
-function rand(items) {
-  if (Number.isInteger(items)) {
-    return Math.floor(Math.random() * items);
-  } else if (Array.isArray(items)) {
-    return items[Math.floor(Math.random() * (items.length - 1))];
-  } else {
-    return null;
+function getToggleRbb() {
+  var on = false;
+  var rbb = require('rbb');
+  return function toggleRbb() {
+    if (on) {
+      rbb.off();
+      on = false;
+    } else {
+      rbb(20);
+      on = true;
+    }
   }
 }
